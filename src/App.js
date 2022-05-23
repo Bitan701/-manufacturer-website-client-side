50e998378d15d3f9896dac9087de08e5112adb62
bitan701 <bitan701@gmail.com>
2022-05-24 01:21:37 +0600

    navigation, customLink, navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
+import About from './components/about/About'
+import Blog from './components/blog/Blog'
+import Home from './components/homepage/Home'
 import Products from './components/product/Products'
+import Navbar from './components/shared/Navbar'
 
 function App() {
 	return (
-		<div style={{ textAlign: 'center' }}>
-			<Products />
+		<div>
+			<Navbar />
+
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/products' element={<Products />} />
+				<Route path='/blog' element={<Blog />} />
+				<Route path='/about' element={<About />} />
+			</Routes>
 		</div>
 	)
 }
