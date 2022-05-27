391e1f10805154219bb29daa48e28f40a592895b
bitan701 <bitan701@gmail.com>
2022-05-27 23:02:41 +0600

    register with google deleted

--- src/components/authorization/Register.jsx
+++ src/components/authorization/Register.jsx
@@ -1,6 +1,5 @@
 import {
 	useCreateUserWithEmailAndPassword,
-	useSignInWithGoogle,
 	useUpdateProfile,
 } from 'react-firebase-hooks/auth'
 import React from 'react'
@@ -9,7 +8,6 @@ import { Link, useLocation, useNavigate } from 'react-router-dom'
 import auth from '../../firebase.init'
 import Loading from '../shared/Loading'
 import { toast } from 'react-toastify'
-import useToken from '../custom/useToken'
 
 const Register = () => {
 	const { register, handleSubmit } = useForm()
@@ -19,29 +17,23 @@ const Register = () => {
 
 	const [updateProfile, updating, updateError] = useUpdateProfile(auth)
 
-	const [signInWithGoogle, guser, gloading, gerror] = useSignInWithGoogle(auth)
-
 	const navigate = useNavigate()
 	const location = useLocation()
 	const from = location?.state?.from?.pathname || '/'
 
-	const [token] = useToken(user || guser)
-
-	if (error || gerror || updateError) {
+	if (error || updateError) {
 		return (
 			<div>
-				<p>
-					Error: {error && error.message} {gerror && gerror.message}
-				</p>
+				<p>Error: {error && error.message}</p>
 			</div>
 		)
 	}
 
-	if (loading || gloading || updating) {
+	if (loading || updating) {
 		return <Loading />
 	}
 
-	if (token) {
+	if (user) {
 		navigate(from, { replace: true })
 	}
 
@@ -58,9 +50,6 @@ const Register = () => {
 	return (
 		<div className='flex justify-center items-center flex-col h-screen gap-8'>
 			<h1 className='text-3xl'>Register</h1>
-			<button onClick={() => signInWithGoogle()} className='btn btn-wide'>
-				sign up with google
-			</button>
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<div className='form-control gap-3'>
 					<label className='input-group'>
