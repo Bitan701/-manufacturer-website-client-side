46cd4b362518c931bdd6f2002172c3d45db5c24d
bitan701 <bitan701@gmail.com>
2022-05-27 22:36:11 +0600

    style update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,20 +3,31 @@ import pic from '../../assets/pic.jpg'
 
 const About = () => {
 	return (
-		<div className='hidden lg:block'>
-			<p className='text-2xl relative top-60 left-10'>
-				<span className=''>Hi, </span> <br />
-				<span className='text-xl'>I am </span>
-				<span className='text-3xl'>BITAN DEBNATH,</span> <br />
-				<span>a freelance </span>
-				<span className='text-3xl font-extrabold'>WEB DEVELOPER</span>
-			</p>
-			<img
-				src={pic}
-				className='w-auto max-w-[1024px]'
-				alt='msi radeon promotion'
-			/>
-		</div>
+		<>
+			<div className='hidden lg:block'>
+				<p className='text-2xl relative top-60 left-10'>
+					<span className=''>Hi, </span> <br />
+					<span className='text-xl'>I am </span>
+					<span className='text-3xl'>BITAN DEBNATH,</span> <br />
+					<span>a freelance </span>
+					<span className='text-3xl font-extrabold'>WEB DEVELOPER</span>
+				</p>
+				<img
+					src={pic}
+					className='w-auto max-w-[1024px]'
+					alt='msi radeon promotion'
+				/>
+			</div>
+			<div className='lg:hidden'>
+				<p className='text-2xl'>
+					<span className=''>Hi, </span> <br />
+					<span className='text-xl'>I am </span>
+					<span className='text-3xl'>BITAN DEBNATH,</span> <br />
+					<span>a freelance </span>
+					<span className='text-3xl font-extrabold'>WEB DEVELOPER</span>
+				</p>
+			</div>
+		</>
 	)
 }
 
